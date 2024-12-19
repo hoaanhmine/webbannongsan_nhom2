f9d6e20c8d36cc8c2f6d6f1168e1a5d6c7f16640
HMFNF <51531073+hoaanhmine@users.noreply.github.com>
2024-12-19 23:17:31 +0700

    test add sql

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'userdb'
+});
+
+db.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to database');
+});
+
+app.post('/register', (req, res) => {
+    const { username, email, password } = req.body;
+    const sql = 'INSERT INTO users (username, email, password) VALUES (?, ?, ?)';
+    db.query(sql, [username, email, password], (err, result) => {
+        if (err) throw err;
+        res.send('User registered');
+    });
+});
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    const sql = 'SELECT * FROM users WHERE username = ? AND password = ?';
+    db.query(sql, [username, password], (err, results) => {
+        if (err) throw err;
+        if (results.length > 0) {
+            res.send('Login successful');
+        } else {
+            res.send('Invalid username or password');
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
