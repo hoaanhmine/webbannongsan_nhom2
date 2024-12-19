8d3a0965df11cc2b35c2ddea5660bf29acadcd01
HMFNF <51531073+hoaanhmine@users.noreply.github.com>
2024-12-19 22:20:55 +0700

    login?

---
+++ login.js
@@ -0,0 +1,11 @@
+document.getElementById('registerForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Đăng ký thành công!');
+    // Thêm code xử lý đăng ký ở đây
+});
+
+document.getElementById('loginForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Đăng nhập thành công!');
+    // Thêm code xử lý đăng nhập ở đây
+});
