87137321d11a19afd93f97adfb923da0b9a71bd1
ChrisSkibidi <phamducchi205@gmail.com>
2024-12-23 09:28:49 +0700

    sua

--- page/sanpham.php
+++ page/sanpham.php
@@ -2,31 +2,31 @@
     <section class="products" class="container">
         <div class="product-slider">
             <div class="product-list">
-                <div class="product-item">
+                <div class="sp">
                     <img src="product1.jpg" alt="Sản phẩm 1">
                     <h3>Sản phẩm 1</h3>
                     <p>Giá: 500,000 VND</p>
                     <button>Thêm vào giỏ hàng</button>
                 </div>
-                <div class="product-item">
+                <div class="sp">
                     <img src="product2.jpg" alt="Sản phẩm 2">
                     <h3>Sản phẩm 2</h3>
                     <p>Giá: 750,000 VND</p>
                     <button>Thêm vào giỏ hàng</button>
                 </div>
-                <div class="product-item">
+                <div class="sp">
                     <img src="product3.jpg" alt="Sản phẩm 3">
                     <h3>Sản phẩm 3</h3>
                     <p>Giá: 1,000,000 VND</p>
                     <button>Thêm vào giỏ hàng</button>
                 </div>
-                <div class="product-item">
+                <div class="sp">
                     <img src="product4.jpg" alt="Sản phẩm 4">
                     <h3>Sản phẩm 4</h3>
                     <p>Giá: 650,000 VND</p>
                     <button>Thêm vào giỏ hàng</button>
                 </div>
-                <div class="product-item">
+                <div class="sp">
                     <img src="product5.jpg" alt="Sản phẩm 5">
                     <h3>Sản phẩm 5</h3>
                     <p>Giá: 800,000 VND</p>
