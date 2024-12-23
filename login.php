bd361b6d2a2c5f86eec4277f8129280d5de66f33
HMFNF <51531073+hoaanhmine@users.noreply.github.com>
2024-12-23 10:14:53 +0700

    sua lai form dang nhap dang ki

---
+++ login.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="./css/login.css">
+<div class="form-container" id="loginForm">
+            <h2>Đăng nhập</h2>
+            <form>
+                <input type="text" placeholder="Tên người dùng hoặc Email" required>
+                <input type="password" placeholder="Mật khẩu" required>
+                <button type="submit">Đăng nhập</button>
+            </form>
+        </div>
+    </div>
+    <script src="login.js"></script>
---
+++ register.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="./css/login.css">
+<div class="container">
+        <div class="form-container" id="registerForm">
+            <h2>Đăng ký</h2>
+            <form>
+                <input type="text" placeholder="Tên người dùng" required>
+                <input type="email" placeholder="Email" required>
+                <input type="password" placeholder="Mật khẩu" required>
+                <button type="submit">Đăng ký</button>
+            </form>
+        </div>
