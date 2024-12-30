ab8d64ff2888363573b1c528c176b6a25f12a988
ChrisSkibidi <phamducchi205@gmail.com>
2024-12-30 11:23:23 +0700

    sua giao dien

--- index.php
+++ index.php
@@ -5,14 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nông Sản HmnFnF</title>
-    <link rel="stylesheet" href="./css/giaodine.css">
+    <link rel="stylesheet" href="giaodine.css">
     <link rel="stylesheet" href="fontawesome-free-6.5.2-web/css/all.css">
 </head>
 <body>
+    <div id="khung">
         <?php include("./page/header.php")?>
         <?php include("./page/baner.php")?>
         <?php include("./page/menu.php")?>
         <?php include("./page/sanpham.php")?>
         <?php include("./page/footer.php")?>
+    </div>
 </body>
 </html>
