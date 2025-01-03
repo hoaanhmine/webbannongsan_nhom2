6a618f2f5f5ee67b0d45c6d3357cdeb9129e0cc0
LeevanToanmck <toankomay@gmail.com>
2025-01-03 11:10:10 +0700

    timkiem

--- page/header.php
+++ page/header.php
@@ -15,9 +15,11 @@
                     <a href="http://localhost/webbannongsan_nhom2/"><img src="./image/Logo_n.png" alt=""></a>
                 </div>
                 <div class="timkiem">
-                   <a href="#"> <input type="text" placeholder="tìm kiếm..."><i class="fa-solid fa-magnifying-glass"></i></a>
-                    
-                </div>
+                    <form action="index.php?quanly=timkiem" method="$_GET">
+                    <input type="text" placeholder="Tìm kiếm..." name="tukhoa">
+                    <input class ="nhan"type="submit" name="timkiem" value="Tìm kiếm">
+                    <!-- <input type="nhan</a> --></form>
+                </div>  
                 <div class="menuphai">
                     <ul>
                         <li><a href="http://localhost/webbannongsan_nhom2/"><i class="fa-solid fa-house"></i>TRANG CHỦ</a></li>
