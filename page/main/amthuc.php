4a151003626ecbccf4ba8d577e87874c4a283835
ChrisSkibidi <phamducchi205@gmail.com>
2025-01-03 14:02:31 +0700

    update giao dien

--- page/main/amthuc.php
+++ page/main/amthuc.php
@@ -1,7 +1,9 @@
-<div class="sanpham"><h2>Sản phẩm tiêu biểu</h2></div>
+<div class="sanpham">Ẩm thực</div>
     <section class="products" class="container">
         <div class="product-slider">
             <div class="danhmucsp">
+                    <!-- san pham trai -->
+                <div class="sptrai">
                 <div class="sp">
                     <img src="./image/anhsp/apple1.jpg" alt="Sản phẩm 1">
                     <h3>Apple</h3>
@@ -26,6 +28,38 @@
                     <p>Giá: 60,000 VND</p>
                     <button>Thêm vào giỏ hàng</button>
                 </div>
+                </div>
+
+                    <!-- san pham giua -->
+                <div class="spgiua">
+                <div class="sp">
+                    <img src="./image/anhsp/apple1.jpg" alt="Sản phẩm 1">
+                    <h3>Apple</h3>
+                    <p>Giá: 10,000 VND</p>
+                    <button>Thêm vào giỏ hàng</button>
+                </div>
+                <div class="sp">
+                    <img src="./image/anhsp/cam1.webp" alt="Sản phẩm 2">
+                    <h3>Orange</h3>
+                    <p>Giá: 15,000 VND</p>
+                    <button>Thêm vào giỏ hàng</button>
+                </div>
+                <div class="sp">
+                    <img src="./image/anhsp/chuoi1.webp" alt="Sản phẩm 3">
+                    <h3>Banana</h3>
+                    <p>Giá: 70,000 VND</p>
+                    <button>Thêm vào giỏ hàng</button>
+                </div>
+                <div class="sp">
+                    <img src="./image/anhsp/duahau1.jpg" alt="Sản phẩm 4">
+                    <h3>watermelon</h3>
+                    <p>Giá: 60,000 VND</p>
+                    <button>Thêm vào giỏ hàng</button>
+                </div>
+
+    <!-- san pham phai -->
+                </div>
+                <div class="spphai">
                 <div class="sp">
                     <img src="./image/anhsp/quadao.jpg" alt="Sản phẩm 5">
                     <h3>peach</h3>
@@ -38,7 +72,19 @@
                     <p>Giá: 10,000 VND</p>
                     <button>Thêm vào giỏ hàng</button>
                 </div>
-                                <!-- Thêm sản phẩm nếu cần -->
+                <div class="sp">
+                    <img src="./image/anhsp/cam1.webp" alt="Sản phẩm 2">
+                    <h3>Orange</h3>
+                    <p>Giá: 15,000 VND</p>
+                    <button>Thêm vào giỏ hàng</button>
+                </div>
+                <div class="sp">
+                    <img src="./image/anhsp/chuoi1.webp" alt="Sản phẩm 3">
+                    <h3>Banana</h3>
+                    <p>Giá: 70,000 VND</p>
+                    <button>Thêm vào giỏ hàng</button>
+                </div>
+                 </div>               <!-- Thêm sản phẩm nếu cần -->
             </div>
         </div>
     </section>
